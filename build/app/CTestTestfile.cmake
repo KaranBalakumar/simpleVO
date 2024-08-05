99018ad9cf119c0898b2f02b7431f64b3d40fa97
Uchiha729 <f20221736@hyderabad.bits-pilani.ac.in>
2024-08-05 15:52:30 +0530

    removed build files

--- build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/karan/simpleVO/app
-# Build directory: /home/karan/simpleVO/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
