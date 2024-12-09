1e077fc98cb55eccab30ddef3cc9184625380e50
Uchiha729 <f20221736@hyderabad.bits-pilani.ac.in>
2024-12-09 20:05:50 +0530

    updated viewer

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/karan/simpleVO/src
-# Build directory: /home/karan/simpleVO/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
